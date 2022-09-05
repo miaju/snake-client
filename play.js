6743e4bb00f463a4bf2288c17c88be8a5820eda4
Mia Tanner <mia@indygecko.com>
2022-09-05 11:59:55 -0700

    add input module

--- play.js
+++ play.js
@@ -1,26 +1,9 @@
 const  { connect } = require("./client");
+const { setupInput } = require("./input");
 
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-setupInput();
 
 console.log("Connecting ...");
 connect();
 
 
-
+setupInput();
