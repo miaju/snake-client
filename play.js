85bddcd03abd54b4b78191528c2c8686c4a6b2a9
Mia Tanner <mia@indygecko.com>
2022-09-05 11:44:15 -0700

    separate module for tcp conection

--- play.js
+++ play.js
@@ -1,20 +1,5 @@
-const net = require("net");
+const  { connect } = require("./client");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: '10.0.0.164', // IP address here,
-    port: 50541// PORT number here,
-  });
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
