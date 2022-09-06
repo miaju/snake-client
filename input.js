69d4ddb90447d2796cc6eaa3e83848e2c12ad106
Mia Tanner <mia@indygecko.com>
2022-09-06 14:27:45 -0700

    add comments

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 let connection;
 const { keyInputs } = require("./constants");
 
+
+// sends key input to the server if it is one of the key binded events
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
@@ -8,7 +10,7 @@ const handleUserInput = function(key) {
   connection.write(keyInputs[key]);
 };
 
-
+// allows to take input from the user and hands it to handleUserInput
 const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
